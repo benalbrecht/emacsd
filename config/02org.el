9e92ad00088895595c2b73b5ae99b54ac4c5bf12
Benjamin Albrecht <git@benalbrecht.de>
2013-11-29 01:45:12 +0100

    set org agenda files

--- config/02org.el
+++ config/02org.el
@@ -3,3 +3,9 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+
+(setq org-agenda-files (list "~/org/work.org"
+                             "~/org/uni.org" 
+                             "~/org/home.org"))
+
+
