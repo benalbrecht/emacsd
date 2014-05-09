4f1f0d4b90699dd54dcb80a3562c39247ff1a9f7
Benjamin Albrecht <git@benalbrecht.de>
2014-05-09 06:50:30 +0200

    begin customizing org mode for gtd

--- config/12org.el
+++ config/12org.el
@@ -21,3 +21,46 @@
 
 ;; smart quotes
 (setq org-export-with-smart-quotes t)
+
+;; org tweaks http://www.brool.com/index.php/using-org-mode-with-gtd
+(setq org-return-follows-link t)
+
+(setq org-agenda-custom-commands
+      '(("w" todo "WAITING" nil)
+        ("n" todo "NEXT" nil)
+        ("d" "Agenda + Next Actions" ((agenda) (todo "NEXT")))
+        ("H" "Office and Home Lists"
+         ((agenda)
+          (tags-todo "OFFICE")
+          (tags-todo "HOME")
+          (tags-todo "COMPUTER")
+          (tags-todo "MOVIE")
+          (tags-todo "READING")))
+        ("D" "Daily Action List"
+         ((agenda "" ((org-agenda-ndays 1)
+                      (org-agenda-sorting-strategy
+                       (quote ((agenda time-up priority-down tag-up))))
+                      (org-deadline-warning-days 0))))))
+)
+
+(defun gtd ()
+   (interactive)
+   (find-file "~/org/gtd.org")
+)
+
+;; capture
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+(define-key global-map "\C-cc" 'org-capture)
+
+(setq org-capture-templates
+ '(("s" "Todo with source link" entry (file+headline "~/org/notes.org" "Tasks")
+    "* TODO %^{Description} %^g\n  %?\n  %i\n  %a\n  Entered on: %U")
+   ("t" "Todo" entry (file+headline "~/org/notes.org" "Tasks")
+    "* TODO %^{Description} %^g\n  %?\n  %i\n  Entered on: %U" )
+   ("j" "Journal" entry (file+datetree "~/org/journal.org")
+    "* %?\n  Entered on %U\n  %i\n  %a")))
+
+(setq org-use-fast-todo-selection t)
+
+(setq org-refile-targets (quote (("gtd.org" :maxlevel . 1)
+                                 ("someday.org" :level . 2))))
