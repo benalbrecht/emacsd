047d1031a367b53c72d3c51602dacaaf8a3125c5
Benjamin Albrecht <git@benalbrecht.de>
2014-05-09 06:46:48 +0200

    put autosaves in backup dir

--- config/10common-setup.el
+++ config/10common-setup.el
@@ -111,3 +111,8 @@
   (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
   (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
 (add-hook 'ido-setup-hook 'ido-define-keys)
+
+
+;; put autosaves in backup dir
+(setq auto-save-file-name-transforms
+      `((".*" ,"~/.emacs.d/backups/" t)))
